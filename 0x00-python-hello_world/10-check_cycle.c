8cc4241a0d6cee272946b36956ba9d9c4eb6ce0a
camia97 <3962@holbertonschool.com>
2021-12-20 14:32:08 -0800

    check cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -8,14 +8,16 @@ int check_cycle(listint_t *list)
 {
 	listint_t *a = list, *b = list;
 
-	if (!list)
-		return (0);
-	while (a->next && b->next->next)
+	if (list)
 	{
-		a = a->next;
-		b = b->next->next;
-		if (a == b)
-			return (1);
+		while (a->next && b->next->next)
+		{
+			a = a->next;
+			b = b->next->next;
+			if (a == b)
+				return (1);
+		}
+		return (0);
 	}
 	return (0);
 }
