18d19735f83895b909665a09a5d9b5f08d2f25f1
camia97 <3962@holbertonschool.com>
2021-12-20 14:19:41 -0800

    check cycle

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * check_cycle - checks if a single linked list has cycle
+ * @list: the single linked list
+ * Return: 0 or 1
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *a = list, *b = list;
+
+	if (!list)
+		return (0);
+	while (a->next && b->next->next)
+	{
+		a = a->next;
+		b = b->next->next;
+		if (a == b)
+			return (1);
+	}
+	return (0);
+}
