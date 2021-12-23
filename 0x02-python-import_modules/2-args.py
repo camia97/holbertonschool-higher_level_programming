89343503cfea17e2f34585cda8e1140d9c68e69f
camia97 <3962@holbertonschool.com>
2021-12-23 11:10:37 -0800

    dynamic script

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-import sys
-if len(sys.argv) == 1:
-    print("{} arguments.".format(0))
-elif len(sys.argv) == 2:
-    print("{} argument:\n1: {}".format(1, sys.argv[1]))
-else:
-    print("{} arguments:".format(len(sys.argv) - 1))
-    for i in range(1, len(sys.argv)):
-        print("{}: {}".format(i, sys.argv[i]))
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) == 1:
+        print("{} arguments.".format(0))
+    elif len(sys.argv) == 2:
+        print("{} argument:\n1: {}".format(1, sys.argv[1]))
+    else:
+        print("{} arguments:".format(len(sys.argv) - 1))
+        for i in range(1, len(sys.argv)):
+            print("{}: {}".format(i, sys.argv[i]))
