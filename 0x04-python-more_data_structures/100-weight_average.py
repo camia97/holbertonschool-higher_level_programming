b9cdf87075be5bbea20f0722e5dc8f2c9c111d4b
camia97 <3962@holbertonschool.com>
2022-01-04 12:39:46 -0800

    weighted average

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,6 +2,8 @@
 def weight_average(my_list=[]):
     prom = 0
     add = 0
+    if not my_list:
+        return 0
     for i in my_list:
         add = i[1] + add
         prom = (i[0] * i[1]) + prom
