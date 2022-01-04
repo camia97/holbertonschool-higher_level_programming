8805720b131305e9e0f0256965fc90994879755f
camia97 <3962@holbertonschool.com>
2022-01-04 07:17:04 -0800

    only differents

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    new_set = set_1.copy()
+    new_set = []
     for i in set_1:
-        for j in set_2:
-            if i != j:
-                set_3 = new_set.append(j)
-    return set_3
+        if i not in set_2:
+            new_set.append(i)
+    for j in set_2:
+        if j not in set_1:
+            new_set.append(j)
+    return new_set
