0db83a1e33753b796f59a74dbb7dea7a76ffa21c
camia97 <3962@holbertonschool.com>
2022-01-18 15:35:59 -0800

    square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Empty class """
+
+
+class Square:
+    """ Class Square"""
+    def __init__(self, size=None):
+        """Private instance attribute"""
+        self.__size = size
