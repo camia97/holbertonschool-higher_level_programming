fd87de5f8cae51f95c9072258ca0e4d54466c521
camia97 <3962@holbertonschool.com>
2022-01-18 16:40:23 -0800

    access and update private attribute

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" Empty class """
+
+
+class Square:
+    """ Class Square"""
+
+    def __init__(self, size=0):
+        """Private instance attribute"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    @property
+    def size(self):
+        """Property"""
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Public instance method"""
+        return self.__size * self.__size
