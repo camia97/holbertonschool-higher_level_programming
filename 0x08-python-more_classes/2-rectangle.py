a83495eebb91d0ae1f527a5e1c8dda8a30973671
camia97 <3962@holbertonschool.com>
2022-01-26 11:16:06 -0800

    area and perimeter

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+""" Def of a rectangle """
+
+
+class Rectangle:
+    """ Class """
+
+    def __init__(self, width=0, height=0):
+        """Private instance attributes """
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        elif width < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = width
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        elif height < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = height
+
+    @property
+    def width(self):
+        """Property width"""
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        elif width < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = width
+
+    @property
+    def height(self):
+        """Property height"""
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        elif height < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = height
+
+    def area(self):
+        """Public instance method area"""
+        return self.__height * self.__width
+
+    def perimeter(self):
+        """Public instance method perimeter"""
+        return 2 * self.__height + 2 * self.__width
