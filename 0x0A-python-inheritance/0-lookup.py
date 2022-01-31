382086d926b8336cd2828206d93fb8f64fd2d6f2
camia97 <3962@holbertonschool.com>
2022-01-31 09:24:43 -0800

    Inheritance

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def lookup(obj):
+    my_list = []
+    return dir(my_list)
---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+class Mylist(list):
+    """Class my list """
+
+        
+    def print_sorted(self):
+        """Print sorted"""
+        print("{}".format(list.sort()))
