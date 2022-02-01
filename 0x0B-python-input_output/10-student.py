9dbc8c22955ed731774fcb581fc188b24a701152
camia97 <3962@holbertonschool.com>
2022-02-01 13:09:19 -0800

    student to json

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""modulo"""
+
+
+class Student:
+    """Class Student"""
+    def __init__(self, first_name, last_name, age):
+        """instantiation"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """public method"""
+        resu = {}
+        if type(attrs) is list:
+            for key in attrs:
+                if type(key) != str:
+                    return self.__dict__
+                if key in self.__dict__:
+                    resu[key] = self.__dict__[key]
+            return resu
+        else:
+            return self.__dict__
