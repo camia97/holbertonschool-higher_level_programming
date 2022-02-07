b6849c1f87c57cc77b4bd8672f00f338de8feae8
camia97 <3962@holbertonschool.com>
2022-02-07 09:38:00 -0800

    change

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -22,13 +22,8 @@ class Square(Rectangle):
 
     @size.setter
     def size(self, size):
-        if type(size) is not int:
-            raise TypeError("size must be an integer")
-        elif size <= 0:
-            raise ValueError("size must be > 0")
-        else:
-            self.width = size
-            self.height = size
+        self.width = size
+        self.height = size
 
     def update(self, *args, **kwargs):
         """Public method update"""
