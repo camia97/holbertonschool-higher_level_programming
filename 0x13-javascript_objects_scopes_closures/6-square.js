c46eb312922d2d9d27ec986c917ccc25797c2be5
camia97 <3962@holbertonschool.com>
2022-04-05 17:28:25 -0300

    task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      let line = '';
+      for (let i = 0; i < this.height; i++) {
+        line += 'C';
+      }
+      for (let j = 0; j < this.width; j++) {
+        console.log(line);
+      }
+    }
+  }
+}
+module.exports = Square;
