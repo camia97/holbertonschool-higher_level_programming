e73b8591d8d85a68c19060737df6ab8f9ed187ba
camia97 <3962@holbertonschool.com>
2022-04-09 15:10:26 -0700

    change pycode

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -12,10 +12,10 @@ if __name__ == "__main__":
         passwd=sys.argv[2],
         db=sys.argv[3]
     )
-cur = db.cursor()
-cur.execute("SELECT * FROM states ORDER BY states.id ASC")
-rows = cur.fetchall()
-for i in rows:
-    print(i)
-cur.close()
-db.close()
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    rows = cur.fetchall()
+    for i in rows:
+        print(i)
+    cur.close()
+    db.close()
