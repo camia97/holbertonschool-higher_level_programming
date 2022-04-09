a0f243ae1837279ae779b553bf0007e0d349505f
camia97 <3962@holbertonschool.com>
2022-04-09 22:26:24 +0000

    add if and binary

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -4,16 +4,17 @@ import MySQLdb
 import sys
 
 
-db = MySQLdb.connect(
-    host="localhost",
-    port=3306,
-    user=sys.argv[1],
-    passwd=sys.argv[2],
-    db=sys.argv[3]
-)
-cur = db.cursor()
-cur.execute("SELECT * FROM `states` WHERE `name`=%s\
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+    cur = db.cursor()
+    cur.execute("SELECT * FROM `states` WHERE `name`= BINARY %s\
             ORDER BY states.id", (sys.argv[4],))
-rows = cur.fetchall()
-for i in rows:
-    print(i)
+    rows = cur.fetchall()
+    for i in rows:
+        print(i)
