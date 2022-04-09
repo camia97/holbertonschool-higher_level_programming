a2f203beb4bae79e62521601975815258aa138a6
camia97 <3962@holbertonschool.com>
2022-04-09 22:28:07 +0000

    add if condition

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -4,19 +4,20 @@ import MySQLdb
 import sys
 
 
-db = MySQLdb.connect(
-    host="localhost",
-    port=3306,
-    user=sys.argv[1],
-    passwd=sys.argv[2],
-    db=sys.argv[3]
-)
-cur = db.cursor()
-cur.execute("SELECT cities.id, states.name, cities.name \
-FROM states \
-JOIN cities \
-ON cities.state_id = states.id \
-ORDER BY states.id")
-rows = cur.fetchall()
-for i in rows:
-    print(i)
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+    cur = db.cursor()
+    cur.execute("SELECT cities.id, states.name, cities.name \
+                FROM states \
+                JOIN cities \
+                ON cities.state_id = states.id \
+                ORDER BY states.id")
+    rows = cur.fetchall()
+    for i in rows:
+        print(i)
