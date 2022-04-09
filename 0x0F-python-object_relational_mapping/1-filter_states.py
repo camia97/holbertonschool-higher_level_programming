4a3d54371f24ce954f7dee8ff06b42e9b402b4e0
camia97 <3962@holbertonschool.com>
2022-04-09 22:12:27 +0000

    change identation

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -3,16 +3,16 @@
 import MySQLdb
 import sys
 
-
-db = MySQLdb.connect(
-    host="localhost",
-    port=3306,
-    user=sys.argv[1],
-    passwd=sys.argv[2],
-    db=sys.argv[3]
-)
-cur = db.cursor()
-cur.execute("SELECT * FROM states WHERE NAME LIKE 'N%' ORDER BY states.id")
-rows = cur.fetchall()
-for i in rows:
-    print(i)
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE NAME LIKE 'N%' ORDER BY states.id")
+    rows = cur.fetchall()
+    for i in rows:
+        print(i)
